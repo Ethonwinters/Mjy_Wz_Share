a0ca489d0291d2637bf0b7ce48d7b37540e0bd42
Ethonwinters <145417125+Ethonwinters@users.noreply.github.com>
2025-04-25 17:18:34 +0800

    Create share.py

---
+++ share.py
@@ -0,0 +1,24 @@
+https://space.bilibili.com/484123810?spm_id_from=333.1391.0.0  松灵机器人
+https://www.bilibili.com/video/BV1nqWoejEvP/?spm_id_from=333.1387.favlist.content.click
+https://www.bilibili.com/video/BV1NAebePETu/?spm_id_from=333.1387.favlist.content.click
+https://www.bilibili.com/video/BV1ZaeAe7EMu/?spm_id_from=333.1387.favlist.content.click
+
+TeleOperate
+https://github.com/OpenTeleVision/TeleVision   
+https://github.com/unitreerobotics/avp_teleoperate
+https://github.com/unitreerobotics  
+
+Stanford Mobile ALOHA
+https://github.com/MarkFzp/act-plus-plus  
+https://github.com/MarkFzp/mobile-aloha
+https://mobile-aloha.github.io/   Projiect Website
+
+Hugging face Lerobot
+https://github.com/huggingface/lerobot
+
+Humanoid
+Omnih2o and humanoid 2 humanoid https://github.com/LeCAR-Lab/human2humanoid
+HOVER https://github.com/NVlabs/HOVER
+
+AnyTeleop: A General Vision-Based Dexterous Robot Arm-Hand Teleoperation System
+https://github.com/dexsuite/dex-retargeting
