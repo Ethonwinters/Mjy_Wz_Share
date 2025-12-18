f66ed5a9aeedc2f90c99c78389a46a94cf19159b
Ethonwinters <145417125+Ethonwinters@users.noreply.github.com>
2025-12-18 09:45:07 +0800

    Update share.py

--- share.py
+++ share.py
@@ -2,3 +2,22 @@
 2.4.1                                                                                                                                                                                                                                                                                                                                                                                                                            
 12.1                                                                                                                                                                                                                                                                                                                                                                                                                             
 True    
+
+
+pip install numpy matplotlib packaging pillow tqdm tensorboard 
+pip install opencv-python
+pip install torch-scatter \
+  -f https://data.pyg.org/whl/torch-2.4.1+cu121.html
+
+pip install torch-geometric
+
+python - << EOF
+import torch
+import torch_scatter
+import torch_geometric
+
+print("torch:", torch.__version__)
+print("cuda:", torch.version.cuda)
+print("scatter cuda:", torch_scatter.is_cuda_available())
+print("pyg version:", torch_geometric.__version__)
+EOF
