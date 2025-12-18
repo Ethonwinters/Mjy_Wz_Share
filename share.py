db73f73faae8f5cbdbc769bb246f72a932723df8
Ethonwinters <145417125+Ethonwinters@users.noreply.github.com>
2025-12-18 09:55:36 +0800

    Update share.py

--- share.py
+++ share.py
@@ -21,3 +21,28 @@ See 'snap info aria2c' for additional versions.
 
 (meshgnn) adminstrator@adminstrator-HP-Z8-G5-Workstation-Desktop-PC:~/GNN/meshGraphNets_pytorch$ sudo apt install aria2 -y
 E: dpkg was interrupted, you must manually run 'sudo dpkg --configure -a' to correct the problem. 
+
+(meshgnn) adminstrator@adminstrator-HP-Z8-G5-Workstation-Desktop-PC:~/GNN/meshGraphNets_pytorch$ aria2c -x 8 -s 8 https://storage.googleapis.com/dm-meshgraphnets/cylinder_flow/train.tfrecord -d data
+Caught Error while parsing environment variable 'all_proxy'
+Exception: [AbstractOptionHandler.cc:69] errorCode=28 We encountered a problem while processing the option '--all-proxy'.
+  -> [OptionHandlerImpl.cc:520] errorCode=1 unrecognized proxy format
+
+
+12/18 09:55:14 [NOTICE] Downloading 1 item(s)
+
+12/18 09:55:14 [ERROR] CUID#7 - Download aborted. URI=https://storage.googleapis.com/dm-meshgraphnets/cylinder_flow/train.tfrecord
+Exception: [AbstractCommand.cc:351] errorCode=1 URI=https://storage.googleapis.com/dm-meshgraphnets/cylinder_flow/train.tfrecord
+  -> [SocketCore.cc:1018] errorCode=1 SSL/TLS handshake failure: The TLS connection was non-properly terminated.
+
+12/18 09:55:14 [NOTICE] Download GID#399894f2041ffe74 not complete: 
+
+Download Results:
+gid   |stat|avg speed  |path/URI
+======+====+===========+=======================================================
+399894|ERR |       0B/s|https://storage.googleapis.com/dm-meshgraphnets/cylinder_flow/train.tfrecord
+
+Status Legend:
+(ERR):error occurred.
+
+aria2 will resume download if the transfer is restarted.
+If there are any errors, then see the log file. See '-l' option in help/man page for details.
