e1f35c927aed50cab3a20fd2695191ba5e76bbe2
Ethonwinters <145417125+Ethonwinters@users.noreply.github.com>
2025-12-19 09:54:23 +0800

    Update a.py

--- a.py
+++ a.py
@@ -1,60 +1,24 @@
-{
-  "simulator": "comsol",
-  "dt": 0.01,
-  "collision_radius": null,
-  "features": {
-    "cells": {
-      "type": "static",
-      "shape": [
-        1,
-        -1,
-        3
-      ],
-      "dtype": "int32"
-    },
-    "mesh_pos": {
-      "type": "static",
-      "shape": [
-        1,
-        -1,
-        2
-      ],
-      "dtype": "float32"
-    },
-    "node_type": {
-      "type": "static",
-      "shape": [
-        1,
-        -1,
-        1
-      ],
-      "dtype": "int32"
-    },
-    "velocity": {
-      "type": "dynamic",
-      "shape": [
-        600,
-        -1,
-        2
-      ],
-      "dtype": "float32"
-    },
-    "pressure": {
-      "type": "dynamic",
-      "shape": [
-        600,
-        -1,
-        1
-      ],
-      "dtype": "float32"
-    }
-  },
-  "field_names": [
-    "cells",
-    "mesh_pos",
-    "node_type",
-    "velocity",
-    "pressure"
-  ],
-  "trajectory_length": 600
-}
+(base) adminstrator@adminstrator-HP-Z8-G5-Workstation-Desktop-PC:~$ aria2c -x 8 -s 8 https://storage.googleapis.com/dm-meshgraphnets/cylinder_flow/train.tfrecord -d data
+Caught Error while parsing environment variable 'all_proxy'
+Exception: [AbstractOptionHandler.cc:69] errorCode=28 We encountered a problem while processing the option '--all-proxy'.
+  -> [OptionHandlerImpl.cc:520] errorCode=1 unrecognized proxy format
+
+
+12/19 09:53:46 [NOTICE] Downloading 1 item(s)
+
+12/19 09:53:47 [ERROR] CUID#7 - Download aborted. URI=https://storage.googleapis.com/dm-meshgraphnets/cylinder_flow/train.tfrecord
+Exception: [AbstractCommand.cc:351] errorCode=1 URI=https://storage.googleapis.com/dm-meshgraphnets/cylinder_flow/train.tfrecord
+  -> [SocketCore.cc:1018] errorCode=1 SSL/TLS handshake failure: The TLS connection was non-properly terminated.
+
+12/19 09:53:47 [NOTICE] Download GID#bc520c6a1a312334 not complete: 
+
+Download Results:
+gid   |stat|avg speed  |path/URI
+======+====+===========+=======================================================
+bc520c|ERR |       0B/s|https://storage.googleapis.com/dm-meshgraphnets/cylinder_flow/train.tfrecord
+
+Status Legend:
+(ERR):error occurred.
+
+aria2 will resume download if the transfer is restarted.
+If there are any errors, then see the log file. See '-l' option in help/man page for details.
