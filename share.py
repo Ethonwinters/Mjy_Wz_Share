fa12abafe97f9b5c5dc11644a638295aa2ce0bd1
Ethonwinters <145417125+Ethonwinters@users.noreply.github.com>
2026-04-20 16:47:45 +0800

    Update share.py

--- share.py
+++ share.py
@@ -3,74 +3,12 @@
 12.1                                                                                                                                                                                                                                                                                                                                                                                                                             
 True    
 
+(rectflow) adminstrator@adminstrator-HP-Z8-G5-Workstation-Desktop-PC:~/Rectified_Flow/RectifiedFlow/ImageGeneration$ pip install jax==0.3.4 jaxlib==0.3.2
+Collecting jax==0.3.4
+  Downloading jax-0.3.4.tar.gz (924 kB)
+     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 925.0/925.0 kB 3.2 MB/s eta 0:00:00
+  Preparing metadata (setup.py) ... done
+ERROR: Ignored the following versions that require a different python version: 0.10.0 Requires-Python >=3.11; 0.4.14 Requires-Python >=3.9; 0.4.15 Requires-Python >=3.9; 0.4.16 Requires-Python >=3.9; 0.4.17 Requires-Python >=3.9; 0.4.18 Requires-Python >=3.9; 0.4.19 Requires-Python >=3.9; 0.4.20 Requires-Python >=3.9; 0.4.21 Requires-Python >=3.9; 0.4.22 Requires-Python >=3.9; 0.4.23 Requires-Python >=3.9; 0.4.24 Requires-Python >=3.9; 0.4.25 Requires-Python >=3.9; 0.4.26 Requires-Python >=3.9; 0.4.27 Requires-Python >=3.9; 0.4.28 Requires-Python >=3.9; 0.4.29 Requires-Python >=3.9; 0.4.30 Requires-Python >=3.9; 0.4.31 Requires-Python >=3.10; 0.4.32 Requires-Python >=3.10; 0.4.33 Requires-Python >=3.10; 0.4.34 Requires-Python >=3.10; 0.4.35 Requires-Python >=3.10; 0.4.36 Requires-Python >=3.10; 0.4.37 Requires-Python >=3.10; 0.4.38 Requires-Python >=3.10; 0.5.0 Requires-Python >=3.10; 0.5.1 Requires-Python >=3.10; 0.5.2 Requires-Python >=3.10; 0.5.3 Requires-Python >=3.10; 0.6.0 Requires-Python >=3.10; 0.6.1 Requires-Python >=3.10; 0.6.2 Requires-Python >=3.10; 0.7.0 Requires-Python >=3.11; 0.7.1 Requires-Python >=3.11; 0.7.2 Requires-Python >=3.11; 0.8.0 Requires-Python >=3.11; 0.8.1 Requires-Python >=3.11; 0.8.2 Requires-Python >=3.11; 0.8.3 Requires-Python >=3.11; 0.9.0 Requires-Python >=3.11; 0.9.0.1 Requires-Python >=3.11; 0.9.1 Requires-Python >=3.11; 0.9.2 Requires-Python >=3.11
+ERROR: Could not find a version that satisfies the requirement jaxlib==0.3.2 (from versions: none)
+ERROR: No matching distribution found for jaxlib==0.3.2
 
-pip install numpy matplotlib packaging pillow tqdm tensorboard 
-pip install opencv-python
-pip install torch-scatter \
-  -f https://data.pyg.org/whl/torch-2.4.1+cu121.html
-
-pip install torch-geometric
-
-
-
-(meshgnn) adminstrator@adminstrator-HP-Z8-G5-Workstation-Desktop-PC:~/GNN/meshGraphNets_pytorch$ aria2c -x 8 -s 8 https://storage.googleapis.com/dm-meshgraphnets/cylinder_flow/train.tfrecord -d data
-Command 'aria2c' not found, but can be installed with:
-sudo snap install aria2c  # version 1.35.0, or
-sudo apt  install aria2   # version 1.36.0-1
-See 'snap info aria2c' for additional versions.
-
-(meshgnn) adminstrator@adminstrator-HP-Z8-G5-Workstation-Desktop-PC:~/GNN/meshGraphNets_pytorch$ sudo apt install aria2 -y
-E: dpkg was interrupted, you must manually run 'sudo dpkg --configure -a' to correct the problem. 
-
-
-
-
-mkdir -p data
-wget https://storage.googleapis.com/dm-meshgraphnets/cylinder_flow/train.tfrecord -P data
-wget https://storage.googleapis.com/dm-meshgraphnets/cylinder_flow/valid.tfrecord -P data
-wget https://storage.googleapis.com/dm-meshgraphnets/cylinder_flow/test.tfrecord  -P data
-
-conda create -n tfrecord python=3.7 -y
-conda activate tfrecord
-pip install "tensorflow<1.15"
-pip install numpy packaging
-
-
-meshgnn) adminstrator@adminstrator-HP-Z8-G5-Workstation-Desktop-PC:~/GNN/meshGraphNets_pytorch$ python train.py
-Simulator model initialized
-Optimizer initialized
-100%|████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 29950/29950 [2:29:03<00:00,  3.35it/s]
-Epoch 1/100 Train Loss: 1.26e-01 Valid Loss: 3.73e-03
--> New best model saved at epoch 1 with valid loss 3.73e-03
-100%|████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 29950/29950 [2:23:40<00:00,  3.47it/s]
-Epoch 2/100 Train Loss: 4.56e-02 Valid Loss: 2.87e-03
-  -> New best model saved at epoch 2 with valid loss 2.87e-03
-  ..........
-
-(base) adminstrator@adminstrator-HP-Z8-G5-Workstation-Desktop-PC:~/GNN/meshGraphNets_pytorch$ nvidia-smi
-Thu Dec 18 17:05:06 2025       
-+---------------------------------------------------------------------------------------+
-| NVIDIA-SMI 535.154.05             Driver Version: 535.154.05   CUDA Version: 12.2     |
-|-----------------------------------------+----------------------+----------------------+
-| GPU  Name                 Persistence-M | Bus-Id        Disp.A | Volatile Uncorr. ECC |
-| Fan  Temp   Perf          Pwr:Usage/Cap |         Memory-Usage | GPU-Util  Compute M. |
-|                                         |                      |               MIG M. |
-|=========================================+======================+======================|
-|   0  NVIDIA RTX 5880 Ada Gene...    Off | 00000000:52:00.0  On |                    0 |
-| 62%   81C    P2             271W / 285W |  23135MiB / 46068MiB |     94%      Default |
-|                                         |                      |                  N/A |
-+-----------------------------------------+----------------------+----------------------+
-                                                                                         
-+---------------------------------------------------------------------------------------+
-| Processes:                                                                            |
-|  GPU   GI   CI        PID   Type   Process name                            GPU Memory |
-|        ID   ID                                                             Usage      |
-|=======================================================================================|
-|    0   N/A  N/A      2831      G   /usr/lib/xorg/Xorg                          698MiB |
-|    0   N/A  N/A      2978      G   /usr/bin/gnome-shell                        143MiB |
-|    0   N/A  N/A      3170      G   ...in/bin/sunloginclient --cmd=autorun       13MiB |
-|    0   N/A  N/A      3367      G   ...) Chrome/58.0.3029.81 Safari/537.36        7MiB |
-|    0   N/A  N/A      3470      G   ...en=45586B388FD74A3ADD009CBA1BCBF6D3        6MiB |
-|    0   N/A  N/A      4330      G   ...irefox/6700/usr/lib/firefox/firefox      374MiB |
-|    0   N/A  N/A     37582      C   python                                    21708MiB |
-|    0   N/A  N/A     38064      G   /proc/self/exe                              143MiB |
