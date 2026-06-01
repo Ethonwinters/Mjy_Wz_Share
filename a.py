56b250f1d8bb85efd4fce761bc1880ff28fbd744
Ethonwinters <145417125+Ethonwinters@users.noreply.github.com>
2026-06-01 15:50:30 +0800

    Update a.py

--- a.py
+++ a.py
@@ -1,1 +1,31 @@
-DataBatch(x=[37155, 3], y=[37155, 2], pos=[37155, 2], face=[3, 69619], batch=[37155], ptr=[21])
+(base) adminstrator@adminstrator-HP-Z8-G5-Workstation-Desktop-PC:~$ nvcc -V
+nvcc: NVIDIA (R) Cuda compiler driver
+Copyright (c) 2005-2023 NVIDIA Corporation
+Built on Tue_Jun_13_19:16:58_PDT_2023
+Cuda compilation tools, release 12.2, V12.2.91
+Build cuda_12.2.r12.2/compiler.32965470_0
+(base) adminstrator@adminstrator-HP-Z8-G5-Workstation-Desktop-PC:~$ nvidia-smi
+Mon Jun  1 15:45:37 2026       
++---------------------------------------------------------------------------------------+
+| NVIDIA-SMI 535.154.05             Driver Version: 535.154.05   CUDA Version: 12.2     |
+|-----------------------------------------+----------------------+----------------------+
+| GPU  Name                 Persistence-M | Bus-Id        Disp.A | Volatile Uncorr. ECC |
+| Fan  Temp   Perf          Pwr:Usage/Cap |         Memory-Usage | GPU-Util  Compute M. |
+|                                         |                      |               MIG M. |
+|=========================================+======================+======================|
+|   0  NVIDIA RTX 5880 Ada Gene...    Off | 00000000:52:00.0  On |                    0 |
+| 30%   35C    P8              12W / 285W |    337MiB / 46068MiB |      0%      Default |
+|                                         |                      |                  N/A |
++-----------------------------------------+----------------------+----------------------+
+                                                                                         
++---------------------------------------------------------------------------------------+
+| Processes:                                                                            |
+|  GPU   GI   CI        PID   Type   Process name                            GPU Memory |
+|        ID   ID                                                             Usage      |
+|=======================================================================================|
+|    0   N/A  N/A      2868      G   /usr/lib/xorg/Xorg                          148MiB |
+|    0   N/A  N/A      3021      G   /usr/bin/gnome-shell                        142MiB |
+|    0   N/A  N/A      3210      G   ...in/bin/sunloginclient --cmd=autorun       13MiB |
+|    0   N/A  N/A      3424      G   ...) Chrome/58.0.3029.81 Safari/537.36        7MiB |
+|    0   N/A  N/A      3516      G   ...en=9AB17541D0E4E22C6E1E17B801AE2547        6MiB |
++---------------------------------------------------------------------------------------+
